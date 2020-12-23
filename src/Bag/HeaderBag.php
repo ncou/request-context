7f3b8ab50b7ee12d904068271a105bb10bb95814
ncou <nicolas_courade@yahoo.fr>
2020-12-23 22:53:15 +0100

    update

--- src/Bag/HeaderBag.php
+++ src/Bag/HeaderBag.php
@@ -44,11 +44,37 @@ final class HeaderBag extends ParameterBag
 
      * @return string
      */
+    // TODO : il faudrait surement aussi enlever le HTTP_ devant le nom.
+    // TODO : déplacer cette méthode dans la future classe Header::class avec une méthode normalize !!!
+    //https://github.com/yiisoft/yii2-framework/blob/b80830c5f8c221b8bd6a830c5b2f45544f82c4d7/filters/Cors.php#L256
+    // TODO : renommer en headerize() ???
     private function normalize(string $name): string
     {
-        $normalized = str_replace(['-', '_'], ' ', $name);
-        $normalized = ucwords(strtolower($normalized));
+        $value = ucwords(str_replace(['-', '_'], ' ', $name));
 
-        return str_replace(' ', '-', $normalized);
+        return str_replace(' ', '-', $value);
+
+        //return str_replace(' ', '', ucwords(strtr($word, '_-', '  ')));  // https://github.com/guzzle/guzzle3/blob/f7778ed85e3db90009d79725afd6c3a82dab32fe/src/Guzzle/Inflection/Inflector.php#L36
     }
+
+    /**
+     * Normalize the given header name into studly-case.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    // TODO : déplacer cette méthode dans la future classe Header::class avec une méthode normalize !!!
+    /*
+    protected static function normalizeHeaderName($name)
+    {
+        return str_replace(' ', '-', ucwords(str_replace('-', ' ', $name)));
+    }*/
+
+/*
+    if (strncmp($name, 'HTTP_', 5) === 0) {
+        $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+        $this->_headers->add($name, $value);
+    }
+    */
+
 }
